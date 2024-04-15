6e4b8295e4411f3f1af50f800bcb64738130186d
Eduado <129158742+EduardoMoro1@users.noreply.github.com>
2024-04-15 08:45:02 -0300

    Checkpoint 15/04

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
-document.querySelector('parametro-senha-texto');
-const numerosenha = document.querySelector('.parametro-senha-texto')
-numerosenha.textContent = 5;
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 12;
 
+
+const botoes = document.querySelectorAll('.parametro-senha-botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){ 
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textContent = tamanhoSenha
+}
+
+console.log(botoes);
